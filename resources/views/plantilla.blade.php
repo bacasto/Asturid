b51f042ad9c1793ca73ef2e9b0656e973e2ce7f1
bacasto <delrosalsergio@gmail.com>
2023-05-29 14:46:03 +0200

    admin productos

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
+
 @section('content')
-    <h1>hola</h1>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-12"></div>
+        </div>
+    </div>
 @endsection
